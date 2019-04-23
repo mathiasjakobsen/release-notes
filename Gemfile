898d501b5bd21999ab3cabe03e2eed34b7f5975f
Mathias Jakobsen <mathiasjakobsen@me.com>
2019-04-23 17:10:46 +0200

    Github backed releases, for an org's repositories

---
+++ Gemfile
@@ -0,0 +1,13 @@
+ruby '2.6.2'
+
+source 'https://rubygems.org'
+
+git_source(:github) do |repo_name|
+  repo_name = "#{repo_name}/#{repo_name}" unless repo_name.include?('/')
+  "https://github.com/#{repo_name}.git"
+end
+
+gem 'byebug'
+gem 'sinatra'
+gem 'redcarpet'
+gem 'graphql-client'
---
+++ app.rb
@@ -0,0 +1,63 @@
+require 'sinatra'
+require 'redcarpet'
+require 'graphql/client'
+require 'graphql/client/http'
+
+OWNER = ENV['OWNER']
+BEARER = ENV['BEARER']
+ALLOWED_IPS = ['89.221.173.158', 'localhost', '127.0.0.1', '::1'].freeze
+
+http = GraphQL::Client::HTTP.new('https://api.github.com/graphql') do
+  def headers(_)
+    {
+      'Authorization' => "bearer #{BEARER}"
+    }
+  end
+end
+
+schema = GraphQL::Client.load_schema(http)
+client = GraphQL::Client.new(schema: schema, execute: http)
+
+QUERY = client.parse <<-'GRAPHQL'
+  query($owner: String!, $name: String!) {
+    repository(owner: $owner, name: $name) {
+      id
+      descriptionHTML
+      releases(first: 25) {
+        totalCount
+        edges {
+          cursor
+          node {
+            description
+            createdAt
+            name
+            publishedAt
+          }
+        }
+      }
+    }
+  }
+GRAPHQL
+
+get '/' do
+  'So.. What about the pope?'
+end
+
+get '/:repo' do
+  return 'No.' unless ALLOWED_IPS.include?(request.ip)
+
+  name = params[:repo]
+  releases = client.query(QUERY, variables: { owner: OWNER, name: name })
+  markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+
+  releases = releases.data.repository.releases.edges.map do |edge|
+    {
+      title: edge.node.name,
+      created_at: Date.iso8601(edge.node.created_at).strftime,
+      published_at: Date.iso8601(edge.node.published_at).strftime,
+      html: markdown.render(edge.node.description)
+    }
+  end
+
+  erb :releases, locals: { releases: releases }
+end
